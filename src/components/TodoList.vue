0e93c246141fd6ec6e7692875e7026fa86c40e0a
osabl <andrii.osabliuk@gmail.com>
2020-05-08 16:36:15 +0300

    add components

---
+++ src/components/TodoList.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="todo-list">
+    <TodoItem v-for="todoItem in todoList"
+      :todo="todoItem"
+      :key="todoItem.id"
+      @remove-todo="removeTodo"
+    />
+  </ul>
+</template>
+
+<script>
+import TodoItem from '@/components/TodoItem.vue'
+
+export default {
+  props: [
+    'todoList'
+  ],
+  components: {
+    TodoItem
+  },
+  methods: {
+    removeTodo (todo) {
+      this.$emit('remove-todo', todo)
+    }
+  }
+}
+</script>
