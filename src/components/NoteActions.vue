e99ab92a3e22e4adc2ccbdfb558241cf0a09c5e7
osabl <andrii.osabliuk@gmail.com>
2020-05-08 16:36:15 +0300

    add components

---
+++ src/components/NoteActions.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="note-actions">
+    <button @click="undo">undo</button>
+    <button @click="redo">redo</button>
+    <button @click="cancel">cancel</button>
+    <button @click="save">save</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    undo () {
+      this.$emit('undo')
+    },
+    redo () {
+      this.$emit('redo')
+    },
+    cancel () {
+      this.$emit('cancel')
+    },
+    save () {
+      this.$emit('save')
+    }
+  }
+}
+</script>
