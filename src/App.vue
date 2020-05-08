04982d22e6b1f46d3fb1469c7a5e2894b9b3257a
osabl <andrii.osabliuk@gmail.com>
2020-05-08 18:05:59 +0300

    remove unnecessary

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <template>
   <div id="app">
-    <h1>Note</h1>
-    <hr />
-    <Note/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import Note from './components/Note.vue'
-// import { mapState } from 'vuex'
 
 export default {
-  name: 'App',
-  components: {
-    Note
-  }
-  // computed: mapState({ note: state => state.notes[0] })
+  name: 'App'
 }
 </script>
 
