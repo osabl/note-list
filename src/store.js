f83111550256d1b168351618a94c6050022bd89f
osabl <andrii.osabliuk@gmail.com>
2020-05-09 00:25:29 +0300

    add local storage support & rename action and mutation

--- src/store.js
+++ src/store.js
@@ -5,37 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    noteList: [{
-      id: 0,
-      title: 'Note 1',
-      list: [
-        { id: 1, title: 'test message 1 from Note 1', completed: false },
-        { id: 2, title: 'test message 2 from Note 1', completed: false },
-        { id: 3, title: 'test message 3 from Note 1', completed: false },
-        { id: 4, title: 'test message 4 from Note 1', completed: false }
-      ]
-    }, {
-      id: 1,
-      title: 'Note 2',
-      list: [
-        { id: 1, title: 'test message 1 from Note 2', completed: false },
-        { id: 2, title: 'test message 2 from Note 2', completed: false },
-        { id: 3, title: 'test message 3 from Note 2', completed: false },
-        { id: 4, title: 'test message 4 from Note 2', completed: false }
-      ]
-    }]
+    noteList: JSON.parse(localStorage.getItem('noteList') || '[]')
   },
   mutations: {
-    createNote (state, note) {
-      state.noteList.push(note)
+    addNote (state, note) {
+      state.noteList.unshift(note)
+      localStorage.setItem('noteList', JSON.stringify(state.noteList))
     },
     updateNote (state, note) {
       state.noteList[state.noteList.findIndex(oldNote => oldNote.id === note.id)] = JSON.parse(JSON.stringify(note))
     }
   },
   actions: {
-    createNote ({ commit }, note) {
-      commit('createNote', note)
+    addNote ({ commit }, note) {
+      commit('addNote', note)
     },
     updateNote ({ commit }, note) {
       commit('updateNote', note)
