9473a6d473badd720580b968aeba85861d1835dc
osabl <andrii.osabliuk@gmail.com>
2020-05-10 22:50:09 +0300

    add styles

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="show" class="modal-mask">
+  <div v-if="show" @click.self="cancel" class="modal-mask">
 
     <div class="modal">
 
@@ -50,7 +50,7 @@ export default {
 <style scoped>
 .modal-mask {
   position: fixed;
-  z-index: 1000;
+  z-index: 100;
   top: 0;
   left: 0;
 
@@ -63,7 +63,11 @@ export default {
 
   background-color: rgba(0, 0, 0, 0.5);
 }
+
 .modal {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  line-height: 1.5em;
+
   margin: 0.7em;
   padding: 20px 30px;
 
@@ -73,10 +77,27 @@ export default {
   background-color: #ffffff;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
 }
+
 .btn {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   font-size: 26px;
 
   margin: 0.25em 0.5em;
+
+  cursor: pointer;
+  transition: 0.2s ease;
+
+  color: rgb(121, 54, 141);
+  border: none;
+  outline: none;
+  background: none;
+
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
+  -webkit-tap-highlight-color: transparent;
+}
+
+.btn:hover {
+  color: rgb(212, 0, 255);
 }
 
 </style>
